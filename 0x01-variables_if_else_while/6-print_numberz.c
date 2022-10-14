640febd3d35c73a1cd46cf097e8e6cd047cf4119
ka-riim <tunde.aa74@gmail.com>
2022-10-14 13:39:34 -0700

    comm

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num;
+
+	num = 0;
+	while (num <= 9)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar("\n");
+	return (0);
+}
