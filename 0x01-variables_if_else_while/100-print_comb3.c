6f0744b54a8d7d4832d02b62c93f97148e61330f
ka-riim <tunde.aa74@gmail.com>
2022-10-14 16:04:46 -0700

    ll

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,16 +8,17 @@ int main(void)
 	int y;
 	int z;
 
-	for (y = 0; z <= 9; y++)
+	for (y = 0; y <= 9; y++)
 	{
-		putchar((y % 10) + '0');
-		putchar((z % 10) + '0');
-		if (y == 8 && z == 9)
+		for (z = y + 1; z < 10; z++)
 		{
-			continue;
+			putchar((y % 10) + '0');
+			putchar((z % 10) + '0');
+			if (y == 8 && z == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
-		putchar(',');
-		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
