b37ebe997325a36da4be4a43883ef6e4201b0907
ka-riim <tunde.aa74@gmail.com>
2022-10-15 12:58:36 -0700

    com

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
- * main -Entry
+ * main - Entry
  * Return: Always 0 (Success)
 */
 int main(void)
 {
 	int a, b, c;
 
-	for (a = 0; a <= 8; a++)
+	for (a = 0; a < 8; a++)
 	{
-		for (b = a + 1; b <= 9; b++)
+		for (b = a + 1; b < 9; b++)
 		{
-			for (c = b + 1; c <= 10; b++)
+			for (c = b + 1; c < 10; c++)
 			{
 				if (a != b && a != c && b != c)
 				{
