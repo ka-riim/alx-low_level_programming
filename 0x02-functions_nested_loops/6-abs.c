7c4a81e75cf949122f6cba01f024f5a64f824307
ka-riim <tunde.aa74@gmail.com>
2022-10-18 15:12:36 -0700

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - checks for absolute value
+ * @p: number for checking
+ * Return: 0 if success
+*/
+int _abs(int p)
+{
+	if (p >= 0)
+	{
+		return (p);
+	}
+	else
+	{
+		return (-p);
+	}
+}
