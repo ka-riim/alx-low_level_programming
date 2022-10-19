67e9e5c518c5c9b6edf257a68f7b5304fcdc74f3
ka-riim <tunde.aa74@gmail.com>
2022-10-19 05:00:37 -0700

    fixed

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * print_times_table - entry
+ * @n: holder
+ * Return: numbers
+*/
+void print_times_table(int n)
+{
+	int x, y, z;
+
+	if (!(n > 15 || n < 0))
+	{
+		for (x = 0; x <= n; x++)
+		{
+			for (y = 0; y <= n; y++)
+			{
+				z = (x * y);
+				if (y != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (z < 10 && y != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((z % 10) + '0');
+				}
+				else if (z >= 10 && z < 100)
+				{
+					_putchar(' ');
+					_putchar((z / 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z <= 100 && y != 0)
+				{
+					_putchar((z / 100) + '0');
+					_putchar((z / 10) % 10 + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					_putchar((z % 100) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
