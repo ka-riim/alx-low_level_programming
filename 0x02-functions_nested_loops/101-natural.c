83c9b86a1c54066912e4d4fcc38de6694e10f373
ka-riim <tunde.aa74@gmail.com>
2022-10-19 05:29:05 -0700

    com

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: 0 as success
+*/
+int main(void)
+{
+	int x, y;
+
+	x = 0;
+	if (x < 1024; x++)
+	{
+		if (x % 3 == 0 || x % 5 == 0)
+		{
+			y += x;
+		}
+		printf("%d\n", y);
+		return (0);
+	}
+}
