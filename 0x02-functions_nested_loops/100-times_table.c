d4b0f2d1ecb2949723608ebee733fc8caff64d6d
ka-riim <tunde.aa74@gmail.com>
2022-10-19 05:06:46 -0700

    op

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,7 +32,7 @@ void print_times_table(int n)
 					_putchar((z / 10) + '0');
 					_putchar((z % 10) + '0');
 				}
-				else if (z <= 100 && y != 0)
+				else if (z >= 100 && y != 0)
 				{
 					_putchar((z / 100) + '0');
 					_putchar((z / 10) % 10 + '0');
@@ -40,7 +40,7 @@ void print_times_table(int n)
 				}
 				else
 				{
-					_putchar((z % 100) + '0');
+					_putchar((z % 10) + '0');
 				}
 			}
 			_putchar('\n');
