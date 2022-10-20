80cd848ca1dabfe9f4c2467ae6584ffbbea884f0
ka-riim <tunde.aa74@gmail.com>
2022-10-20 10:36:37 -0700

    com

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - entry
+ * @c: holder
+ * Return: 1 if success
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
