0ad2123e6b805993e7741f84105b9066fb37d786
ka-riim <tunde.aa74@gmail.com>
2022-10-20 14:45:15 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - entry
+*/
+void more_numbers(void)
+{
+	int x = 0;
+	int p;
+
+	while (x <= 9)
+	{
+		p = 0;
+		while (p < 15)
+		{
+			if (p > 9)
+			{
+				_putchar((p / 10) + '0');
+			}
+			_putchar((p % 10) + '0');
+			p++;
+		}
+		x++;
+	}
+	_putchar('\n');
+}
