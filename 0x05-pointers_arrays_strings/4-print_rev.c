a477291a2b7431861cf5635f5211bbf196da3e8f
ka-riim <tunde.aa74@gmail.com>
2022-10-24 14:59:00 -0700

    com

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - entry
+ * @s: integer
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (i -= 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
