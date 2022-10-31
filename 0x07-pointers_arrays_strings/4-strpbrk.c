c7284c940166a132016eba6c249104d70f7ac069
ka-riim <tunde.aa74@gmail.com>
2022-10-31 12:14:35 -0700

    comm

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -14,9 +14,9 @@ char *_strpbrk(char *s, char *accept)
 		for (i = 0; accept[i] != '\0'; i++)
 		{
 			if (*s == accept[i])
-				return(s);
+				return (s);
 		}
 		s++;
 	}
-	return(0);
+	return (0);
 }
