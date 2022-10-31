de55dbef905506bcb45b506ee2298097d5dd9302
ka-riim <tunde.aa74@gmail.com>
2022-10-31 12:11:06 -0700

    comm

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strpbrk - searches for a string
+ * @s: string to be searched
+ * @accept: str being searched for
+ * Return: char
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	while (s != '\0')
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			if (*s == accept[i])
+				return(s);
+		}
+		s++;
+	}
+	return(0);
+}
