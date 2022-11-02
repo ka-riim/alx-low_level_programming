4cbbe895c154d259a16fe5e0d47746cc78b17da3
ka-riim <tunde.aa74@gmail.com>
2022-11-02 13:15:34 -0700

    commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * is_prime_number - checks for a prime number
+ * @n: number to check
+ * Return: integer
+*/
+int is_prime_number(int n)
+{
+	if (n % n == 0 && n % 1 == 0)
+	{
+		return (1);
+	}
+	return (0);
+}
