5969c83176f9caee7dcc7cb28216f3c57f932deb
ka-riim <tunde.aa74@gmail.com>
2022-11-02 13:05:45 -0700

    com

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns x to the power y
+ * @x: number to raise
+ * @y: power to raise to
+ * Return: power of x to y
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * (_pow_recursion(x, y - 1)));
+}
