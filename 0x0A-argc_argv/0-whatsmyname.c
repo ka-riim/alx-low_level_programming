ddadf46ba83a5ab51e334c8b0b87236a5d263deb
ka-riim <tunde.aa74@gmail.com>
2022-11-04 15:50:27 -0700

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry
+ * @argv: vector
+ * @argc: counter
+ * Return: int
+*/
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
