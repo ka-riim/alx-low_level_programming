6ca5141083926735be0a8e291b476200caa12876
ka-riim <tunde.aa74@gmail.com>
2022-11-08 01:36:34 -0800

    update dir

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - concatenates all arguments of program.
+ * @av: array containing string of argument
+ * @ac: number of arguments passed
+ * Return: pointer to cincatenated string of arguments
+*/
+char *argstostr(int ac, char **av)
+{
+	int i, j, len, blen;
+	char *s;
+	if (ac == 0)
+		return (NULL);
+	if (av == NULL)
+		return (NULL);
+	i = j = len = blen = 0;
+	for (i = 0; av[i]; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			len++;
+	}
+	s = (char *)malloc(len * sizeof(char) + ac + 1);
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; av[i]; i++)
+	{
+		for (j = 0; av[i][j]; j++, clen++)
+			s[blen] = av[i][j];
+		s[blen] = '\n';
+		blen++;
+	}
+	s[blen] = '\0';
+	return (s);
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * strtow - splits a string into words.
+ * @str: string to split
+ * Return: pointer to splitted string
+*/
+char **strtow(char *str)
+{
+}
