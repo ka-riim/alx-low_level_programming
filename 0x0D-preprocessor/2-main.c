8997757d36ffff1dd64868da06a500f1ec68b9a2
ka-riim <tunde.aa74@gmail.com>
2022-11-14 02:59:45 -0800

    code for exe name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0 if success
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
