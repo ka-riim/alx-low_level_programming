7d915c287d2a3fa49eb3e270333f0cc59ff67f55
ka-riim <tunde.aa74@gmail.com>
2022-12-05 04:49:06 -0800

    fixed compilation error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,7 @@
  * @letters: Number of letters to reads and prints
  * Return: number of letters it could read and print
 */
-ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, readed;
 	char *buff = malloc(sizeof(char *) * letters);
